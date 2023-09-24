6346b83a44417f4952d47b764d83486063aec644
Felix Freitas Jr <118613377+FelixFreitasJr@users.noreply.github.com>
2023-09-24 17:36:13 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -44,3 +44,37 @@
             var cadastro = document.getElementById("cadastro");
             cadastro.style.display = "block";
         }
+
+        // Função para validar o CPF no lado do cliente
+        function validarCPF(cpf) {
+            cpf = cpf.replace(/[^\d]+/g, ''); // Remove caracteres não numéricos
+            if (cpf.length !== 11 || !Array.from(cpf).every(digit => digit === cpf[0])) {
+                alert("CPF inválido");
+                return false;
+            }
+            let sum = 0;
+            for (let i = 0; i < 9; i++) {
+                sum += parseInt(cpf.charAt(i)) * (10 - i);
+            }
+            let remainder = 11 - (sum % 11);
+            if (remainder === 10 || remainder === 11) {
+                remainder = 0;
+            }
+            if (remainder !== parseInt(cpf.charAt(9))) {
+                alert("CPF inválido");
+                return false;
+            }
+            sum = 0;
+            for (let i = 0; i < 10; i++) {
+                sum += parseInt(cpf.charAt(i)) * (11 - i);
+            }
+            remainder = 11 - (sum % 11);
+            if (remainder === 10 || remainder === 11) {
+                remainder = 0;
+            }
+            if (remainder !== parseInt(cpf.charAt(10))) {
+                alert("CPF inválido");
+                return false;
+            }
+            return true;
+        }
