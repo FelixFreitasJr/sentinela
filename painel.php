c1693b847903640544a46e4bb0025ee3bc34da45
Felix Freitas Jr <118613377+FelixFreitasJr@users.noreply.github.com>
2023-09-24 16:45:24 -0300

    Update painel.php

--- painel.php
+++ painel.php
@@ -23,16 +23,45 @@ include('verifica_login.php');
         </header>
         <nav>
             <ul class="menu">
-                <li>painel de controle</li>
-                <li>consulta</li>
-                <li>cadastro</li>
-                <li> <a href="logout.php">Sair</a></li>
+                <li><a href="">Painel de controle</a></li>
+                <li><a href="">Consulta</a></li>
+                <li><a href="">Cadastro</a></li>
+                <li><a href="logout.php">Sair</a></li>
             </ul>          
     </nav>
         <section>
         <h1><p id="data-hora"></p></h1>
-        
-        <p>campo para digitar o cpf<br>
+            <!-- campo para digitar o cpf -->
+        <form action="consultar.php" method="post">
+            <label for="cpf">CPF:</label>
+            <input type="text" id="cpf" name="cpf" maxlength="14" placeholder="Informe o CPF" required>
+            <input type="submit" value="Consultar">
+        </form>
+            
+         // Verifique se a consulta retornou resultados
+    <?php
+    include('consultar.php');
+    if ($resultado->num_rows > 0) {
+        // Exiba os resultados
+        while ($row = $resultado->fetch_assoc()) {
+            echo "Nome: " . $row['nome'] . "<br>";
+            echo "Endereço: " . $row['endereco'] . "<br>";
+            echo "Telefone: " . $row['telefone'] . "<br>";
+        }
+    } else {
+        echo "Nenhum resultado encontrado para o CPF: $cpf";
+        // Mostrar seção de cadastro
+        echo '<div id="cadastro" style="display: none;">';
+        echo '<h2>Cadastrar novo cliente</h2>';
+        // Aqui você pode adicionar o formulário de cadastro
+        // por exemplo, nome, endereço, telefone, etc.
+        echo '</div>';
+        echo '<button onclick="mostrarCadastro()">Cadastrar</button>';
+    }
+
+    // Feche a conexão com o banco de dados
+    $conexao->close();
+    ?>
             retorno dos dados informados pelo cpf<br>
             se nao encontrado, abrir formulario para cadastro novo, usando o cpf informado<br>
             botao para salvar apos o prencimento do cadastro
