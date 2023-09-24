b327161e5eeb350bf9a780f4404cabc329bfe63d
Felix Freitas Jr <felixfreitasjr@gmail.com>
2023-09-23 22:00:23 -0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   css/estilo.css
    	modified:   index.php
    	modified:   painel.php

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Felix Freitas Junior">
+    <meta name="description" content="Sistema de controle do almoxarifado">
     <title>Login</title>
 </head>
 
--- painel.php
+++ painel.php
@@ -4,3 +4,18 @@ include('verifica_login.php');
 ?>
 <h2>Olá, <?php print_r($_SESSION['email']);?></h2>
 <h2><a href="logout.php">Sair</a></h2>
+
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Painel de Controle</title>
+    <meta name="author" content="Felix Freitas Junior">
+    <meta name="description" content="Sistema de controle do almoxarifado">
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+    
+</body>
+</html>
